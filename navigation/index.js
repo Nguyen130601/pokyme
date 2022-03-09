7297f05ad1c5013b37031cc7ae29591455db1c3f
jvldwin <Mathematiciannguyen@gmail.com>
2022-03-09 22:00:16 +0700

    Updated bottom tab navigator

--- navigation/index.js
+++ navigation/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 
-import { Image } from 'react-native'
+import { Image, View } from 'react-native'
 
 import logo from '../assets/icons/cube_icon.png'
 
@@ -8,13 +8,79 @@ import HomeScreen from '../screens/HomeScreen'
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
+import Svg, { Path } from 'react-native-svg'
+
 import { icons, COLORS } from '../constants'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const Tab = createBottomTabNavigator()
 
+const TabBarCustomButton = ({ accessibilityState, children, onPress }) => {
+    const selected = true
+    if (selected) {
+        return (
+            <View>
+                <View>
+                    <View></View>
+                    <Svg
+                        width={70}
+                        height={61}
+                        viewBox="0 0 75 61"
+                    >
+                        <Path
+                            d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
+                            fill={COLORS.white}
+                        />
+                    </Svg>
+                    <View style={{ flex: 1, backgroundColor: COLORS.white }}></View>
+                </View>
+
+                <TouchableOpacity
+                    style={{
+                        top: -22.5,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: 50,
+                        height: 50,
+                        borderRadius: 25,
+                        backgroundColor: COLORS.white
+                    }}
+                    onPress={onPress}
+                >
+                    {children}
+                </TouchableOpacity>
+            </View>
+        )
+    } else {
+        return (
+            <TouchableOpacity
+                style={{
+                    flex: 1,
+                    height: 60,
+                    backgroundColor: COLORS.white
+                }}
+                activeOpacity={1}
+                onPress={onPress}
+            >
+                {children}
+            </TouchableOpacity>
+        )
+    }
+}
+
 export default Tabs = () => {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    borderTopWidth: 0,
+                    backgroundColor: "transparent",
+                    elevation: 0
+                }
+            }}
+            initialRouteName='TabOne'
+        >
             <Tab.Screen 
                 name='TabOne'
                 component={HomeScreen}
@@ -28,6 +94,11 @@ export default Tabs = () => {
                                 height: 25,
                                 tintColor: focused ? COLORS.primary : COLORS.secondary
                             }}
+                        />
+                    ),
+                    tabBarButton: ({props}) => (
+                        <TabBarCustomButton 
+                            {...props}
                         />
                     )
                 }}
